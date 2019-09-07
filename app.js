34faad6f92d3b39b76cb32b8c168c7ae9e7939ae
Mohammed Bilal <mohammedbilal1995@gmail.com>
2019-09-07 16:16:36 +0530

    added express

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-const http = require('http')
+var express = require('express');  
+var app = express();  
+var bodyParser = require('body-parser');  
+// Create application/x-www-form-urlencoded parser  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-const server = http.createServer(function(request, response) {
+app.use(function (req, res, next) {
+    var allowedOrigins = [
+        "http://127.0.0.1:3000",
+        "http://localhost:3000"
+    ];
 
-  if (request.method == 'POST') {
-    var body = ''
-    request.on('data', function(data) {
-      body += data
-    })
-    request.on('end', function() {
-      console.log('Body: ' + body)
-      response.writeHead(200, {'Content-Type': 'text/html'})
-      response.end('post received')
-    })
-  } 
-})
+    var origin = req.headers.origin;
+    if (allowedOrigins.indexOf(origin) > -1) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
 
-const port = 3000
-const host = 'localhost'
-server.listen(port, host)
-console.log(`Listening at http://${host}:${port}`)
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,Content-Type,X-Auth-Token"
+    );
+    res.setHeader('Access-Control-Allow-Origin','*');
+
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    if ("OPTIONS" == req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+app.post('/', function (req, res) {  
+   // Prepare output in JSON format  
+   console.log(req.body); 
+   res.sendStatus(200) 
+})  
+var server = app.listen(3000, function () {  
+  var host = "localhost"  
+  var port = server.address().port  
+  console.log("Example app listening at http://%s:%s", host, port)  
+})  
