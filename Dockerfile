f28d3225c7b3d2aa9843f4fef09594b0be11ef6e
Mohammed Bilal <mohammedbilal1995@gmail.com>
2019-09-07 18:11:59 +0530

    stub socket

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 3000
+EXPOSE 4000
 
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -37,13 +37,24 @@ app.use(function (req, res, next) {
         next();
     }
 });
-app.post('/', function (req, res) {  
-   // Prepare output in JSON format  
-   console.log(req.body); 
-   res.sendStatus(200) 
-})  
-var server = app.listen(3000, function () {  
-  var host = "localhost"  
-  var port = server.address().port  
-  console.log("Example app listening at http://%s:%s", host, port)  
-})  
+
+const WebSocket = require('ws')
+ 
+const wss = new WebSocket.Server({ port: 4000 })
+ 
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+})
+// app.post('/', function (req, res) {  
+//    // Prepare output in JSON format  
+//    console.log(req.body); 
+//    res.sendStatus(200) 
+// })  
+// var server = app.listen(3000, function () {  
+//   var host = "localhost"  
+//   var port = server.address().port  
+//   console.log("Example app listening at http://%s:%s", host, port)  
+// })  
