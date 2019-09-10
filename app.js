ffca803283637d13763a45267edf725c70c22ec6
Mohammed Bilal <mohammedbilal1995@gmail.com>
2019-09-10 17:06:22 +0530

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(function (req, res, next) {
+  var allowedOrigins = [
+      "http://127.0.0.1:3000",
+      "http://localhost:3000"
+  ];
 
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,X-Auth-Token"
+  );
+  res.setHeader('Access-Control-Allow-Origin','*');
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  if ("OPTIONS" == req.method) {
+      res.sendStatus(200);
+  } else {
+      next();
+  }
+});
 // const WebSocket = require('ws')
  
 // const wss = new WebSocket.Server({ port: 4000 })
