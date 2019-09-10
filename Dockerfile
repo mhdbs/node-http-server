08af57656e3e33758f710c049a76053d29d3d02d
Mohammed Bilal <mohammedbilal1995@gmail.com>
2019-09-10 13:46:38 +0530

    update port

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 4000
+EXPOSE 3000
 
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -7,54 +7,23 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use(function (req, res, next) {
-    var allowedOrigins = [
-        "http://127.0.0.1:3000",
-        "http://localhost:3000"
-    ];
-
-    var origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
-    }
-
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,Content-Type,X-Auth-Token"
-    );
-    res.setHeader('Access-Control-Allow-Origin','*');
-
-    res.setHeader("Access-Control-Allow-Credentials", true);
-
-    if ("OPTIONS" == req.method) {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
-const WebSocket = require('ws')
+// const WebSocket = require('ws')
  
-const wss = new WebSocket.Server({ port: 4000 })
+// const wss = new WebSocket.Server({ port: 4000 })
  
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  ws.send('Hello! Message From Server!!')
-})
-// app.post('/', function (req, res) {  
-//    // Prepare output in JSON format  
-//    console.log(req.body); 
-//    res.sendStatus(200) 
-// })  
-// var server = app.listen(3000, function () {  
-//   var host = "localhost"  
-//   var port = server.address().port  
-//   console.log("Example app listening at http://%s:%s", host, port)  
-// })  
+// wss.on('connection', ws => {
+//   ws.on('message', message => {
+//     console.log(`Received message => ${message}`)
+//   })
+//   ws.send('Hello! Message From Server!!')
+// })
+app.post('/', function (req, res) {  
+   // Prepare output in JSON format  
+   console.log(req.body); 
+   res.sendStatus(200) 
+})  
+var server = app.listen(3000, function () {  
+  var host = "localhost"  
+  var port = server.address().port  
+  console.log("Example app listening at http://%s:%s", host, port)  
+})  
